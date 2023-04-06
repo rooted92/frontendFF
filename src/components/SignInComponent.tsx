864960cca0bd4e5dc67697c37b54720304033faf
Pedro Castaneda <petecast76@gmail.com>
2023-04-06 00:44:30 -0700

    Fixed isue with columns

--- src/components/SignInComponent.tsx
+++ src/components/SignInComponent.tsx
@@ -6,23 +6,24 @@ import {Container, Row, Col} from 'react-bootstrap';
 function SignIn() {
 
     return(
-        <>
-            <Container>
-                <Row className="">
-                    <Col xs={6}>
-                        test
-                        <Row>
-                            <Col >
-                                <p className="col-4 text-center bg-dark">Test</p>                                    
+        <div>
+            <Container fluid>
+                <Row >
+                    <Col xs={5} className="align-self-center">
+                        <Row className="justify-content-center">
+                            <h1 className="text-center">FleetFinder</h1>
+                            <Col className="col-4 align justify-content-center rounded loginBox">
+                                <input type="text" className="my-5" />
+                                <input type="text" className="mb-3" />                                
                             </Col>
                         </Row>
                     </Col>
-                    <Col xs={6}>
-                        test
+                    <Col xs={7} className="splashImg p-0">
+                        
                     </Col>
                 </Row>
             </Container>
-        </>
+        </div>
     );
 }
 
