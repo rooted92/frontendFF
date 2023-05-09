fb3b57d77c907694550cfe38d210bd4d0a187d42
Pedro Castaneda <petecast76@gmail.com>
2023-05-09 15:36:35 -0700

    Added navigation, improved layout

--- src/components/driveraccount/SubmitTrailerCount.tsx
+++ src/components/driveraccount/SubmitTrailerCount.tsx
@@ -1,12 +1,13 @@
-import React, {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import '../../index.css'
-import { Row, Col, Navbar, Container, Nav, NavDropdown, Card, Button, Form } from 'react-bootstrap'
-import Logo from '../../assets/fleetlogo.png'
-import Bell from '../../assets/Bell.png'
+import { Row, Col, Container, Card, Button, Form } from 'react-bootstrap'
 import NavbarComponent from '../NavbarComponent'
-import Footer from '../FooterComponent'
+import Footer from '../FooterComponent';
+import { useNavigate } from 'react-router-dom'
 
 export default function SubmitTrailerCount() {
+
+    let navigate = useNavigate();
 
     const [userInfo, setUserInfo] = useState({
         id: undefined,
@@ -25,117 +26,128 @@ export default function SubmitTrailerCount() {
         }
     }, []);
 
+    const handleCancelSubmission = () => {
+        navigate('/DriverDashboard');
+    }
+
+    const handleSubmitForm = () => {
+        navigate('/ThankYouForSubmission');
+    }
+
+    const handleAddTrailer = () => {
+        console.log('trailer added!');
+    }
+
+    const handleAddNewLocation = () => {
+        console.log('New location form opened');
+    }
+
     return (
-        <div>
-            <NavbarComponent accountType={userInfo.accountType} />
+        <div className='pageContainer'>
+            <div className='mainContent'>
+                <NavbarComponent accountType={userInfo.accountType} />
 
-            <Container>
-                <Row>
-                    <Col className='pt-3 d-flex justify-content-end'>
-                        <Button variant='danger'>Cancel</Button>
-                    </Col>
-                </Row>
-            </Container>
+                <Container>
+                    <Row>
+                        <Col className='pt-3 d-flex justify-content-end'>
+                            <Button onClick={handleCancelSubmission} variant='danger'>Cancel</Button>
+                        </Col>
+                    </Row>
+                </Container>
 
-            <Container>
-                <Row className='d-flex justify-content-center pt-5'>
-                    <Col className='d-flex justify-content-center'>
-                        <Card style={{ width: '33rem', height: '31rem' }}>
-                            <Card.Body>
-                                <Card.Title className='text-center trfCuz'> Trailer Count Form </Card.Title>
-                                <Form.Group as={Col} className="col-12 mb-3" controlId="accountType">
-                                    <Form.Select className="inputFieldStyle" defaultValue="Location">
-                                        <option>Location</option>
-                                        <option>Dispatcher</option>
-                                        <option>Driver</option>
-                                        <option>Organization</option>
-                                    </Form.Select>
-                                </Form.Group>
-                                <Row>
-                                    <Col>
-                                        <Form.Group controlId="firstName">
-                                            <Form.Control className="inputFieldStyle" type="text" placeholder="Trailer Name" />
-                                        </Form.Group>
-                                    </Col>
-                                    <Col>
-                                        <Form.Group controlId="lastName">
-                                            <Form.Control className="inputFieldStyle" type="text" placeholder="Type" />
-                                        </Form.Group>
-                                    </Col>
-                                </Row>
+                <Container>
+                    <Row className='d-flex justify-content-center pt-5 mb-5'>
+                        <Col className='d-flex justify-content-center'>
+                            <Card style={{ width: '33rem', height: '31rem' }}>
+                                <Card.Body>
+                                    <Card.Title className='text-center trfCuz'> Trailer Count Form </Card.Title>
+                                    <Form.Group as={Col} className="col-12 mb-3" controlId="accountType">
+                                        <Form.Select className="inputFieldStyle" defaultValue="Location">
+                                            <option>Location</option>
+                                            {/* Here we will have a dropdown of all yards to users organization */}
+                                        </Form.Select>
+                                    </Form.Group>
+                                    <Row>
+                                        <Col>
+                                            <Form.Group controlId="firstName">
+                                                <Form.Control className="inputFieldStyle" type="text" placeholder="Trailer Name" />
+                                            </Form.Group>
+                                        </Col>
+                                        <Col>
+                                            <Form.Group controlId="lastName">
+                                                <Form.Control className="inputFieldStyle" type="text" placeholder="Type" />
+                                            </Form.Group>
+                                        </Col>
+                                    </Row>
 
-                                <Row className='pt-3'>
-                                    <Col>
-                                        <Form.Group controlId="firstName">
-                                            <Form.Control className="inputFieldStyle" type="text" placeholder="Empty/Loaded" />
-                                        </Form.Group>
-                                    </Col>
-                                    <Col>
-                                        <Form.Group controlId="lastName">
-                                            <Form.Control className="inputFieldStyle" type="text" placeholder="Clean/Dirty" />
-                                        </Form.Group>
-                                    </Col>
-                                </Row>
+                                    <Row className='pt-3'>
+                                        <Col>
+                                            <Form.Group controlId="firstName">
+                                                <Form.Control className="inputFieldStyle" type="text" placeholder="Empty/Loaded" />
+                                            </Form.Group>
+                                        </Col>
+                                        <Col>
+                                            <Form.Group controlId="lastName">
+                                                <Form.Control className="inputFieldStyle" type="text" placeholder="Clean/Dirty" />
+                                            </Form.Group>
+                                        </Col>
+                                    </Row>
 
-                                <Row className='pt-3'>
-                                    <Col>
-                                        <Form.Group controlId="firstName">
-                                            <Form.Control className="inputFieldStyle" type="text" placeholder="Fuel Level" />
-                                        </Form.Group>
-                                    </Col>
-                                    <Col>
-                                        <Form.Group controlId="lastName">
-                                            <Form.Control className="inputFieldStyle" type="text" placeholder="Length" />
-                                        </Form.Group>
-                                    </Col>
-                                </Row>
+                                    <Row className='pt-3'>
+                                        <Col>
+                                            <Form.Group controlId="firstName">
+                                                <Form.Control className="inputFieldStyle" type="text" placeholder="Fuel Level" />
+                                            </Form.Group>
+                                        </Col>
+                                        <Col>
+                                            <Form.Group controlId="lastName">
+                                                <Form.Control className="inputFieldStyle" type="text" placeholder="Length" />
+                                            </Form.Group>
+                                        </Col>
+                                    </Row>
 
-                                <Row className='pt-3'>
-                                    <Col>
-                                        <Form.Group controlId="lastName">
-                                            <Form.Control style={{height: '5rem'}} className="inputFieldStyle" type="text" placeholder="Additional details..." />
-                                        </Form.Group>
-                                    </Col>
-                                </Row>
+                                    <Row className='pt-3'>
+                                        <Col>
+                                            <Form.Group controlId="lastName">
+                                                <Form.Control style={{ height: '5rem' }} className="inputFieldStyle" type="text" placeholder="Additional details..." />
+                                            </Form.Group>
+                                        </Col>
+                                    </Row>
 
-                                <Row>
-                                    <Col className='pt-4 d-flex justify-content-end'>
-                                    <Button>Add Trailer</Button>
-                                    </Col>
-                                </Row>
+                                    <Row className='justify-content-center gap-2'>
+                                        <Col className='col-4 pt-4 d-flex justify-content-end'>
+                                            <Button onClick={handleAddTrailer} type='button'>Add Trailer</Button>
+                                        </Col>
+                                        {/* <Col className='col-4 pt-4 d-flex justify-content-center'>
+                                            <Button className='btn-primary' onClick={handleAddNewLocation} >Add New Location</Button>
+                                        </Col> */}
+                                    </Row>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col className='d-flex justify-content-center'>
+                            <Card style={{ width: '25rem', height: '31rem' }}>
+                                <Card.Body>
+                                    <Card.Title className='text-center trfCuz'>Trailers Added</Card.Title>
+                                    <Row className='lineCuz'>
+                                        <Col>
+                                            <hr ></hr>
+                                        </Col>
+                                    </Row>
 
+                                    <Row>
+                                        <Col className='fCuz'>
 
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col className='d-flex justify-content-center'>
-                        <Card style={{ width: '25rem', height: '31rem' }}>
-                            <Card.Body>
-                                <Card.Title className='text-center trfCuz'>Trailers Added</Card.Title>
-                                <Row className='lineCuz'>
-                                    <Col>
-                                     <hr ></hr>
-                                    </Col>
-                                </Row>
-                               
-                               <Row>
-                                <Col className='fCuz'>
-                                   
-                                <Button className='buttonColor'>Finish</Button>
-                                </Col>
-                               </Row>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-
-            
-                <Row className='footCuz'>
-                    <Col>
-                    <Footer />
-                    </Col>
-                </Row>
+                                            <Button onClick={handleSubmitForm} className='buttonColor'>Finish</Button>
+                                        </Col>
+                                    </Row>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+            <Footer />
         </div>
     )
 }
