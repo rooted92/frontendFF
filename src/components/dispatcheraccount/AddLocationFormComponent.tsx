0c6f9ff72cbf4be161f936bebe12972b79067247
Pedro Castaneda <petecast76@gmail.com>
2023-05-17 23:59:55 -0700

    Added loading indicator to location form

--- src/components/dispatcheraccount/AddLocationFormComponent.tsx
+++ src/components/dispatcheraccount/AddLocationFormComponent.tsx
@@ -4,6 +4,7 @@ import { Col, Container, Row, Form, Button } from "react-bootstrap";
 import NavbarComponent from "../NavbarComponent";
 import { useState, useEffect } from "react";
 import { AddNewLocation } from "../../services/DataService";
+import LoadingPageComponent from "../LoadingPageComponent";
 
 
 const AddLocationForm = () => {
@@ -31,18 +32,19 @@ const AddLocationForm = () => {
     const [city, setCity] = useState<string>('');
     const [state, setState] = useState<string>('');
     const [zip, setZip] = useState<string>('');
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
     const handleCancel = () => {
         // checking witch account is logged in to route to correct dashboard
-        if(userInfo.accountType === 'Dispatcher'){
-           navigate('/DispatchDashboard'); 
+        if (userInfo.accountType === 'Dispatcher') {
+            navigate('/DispatchDashboard');
         } else if (userInfo.accountType === 'Admin') {
             navigate('/AdminDashboard');
         }
-        
     }
 
     const handleAddLocation = async () => {
+        setIsLoading(true);
         console.log('submitted');
         console.log('OrG ID', userInfo.organizationID);
         let yardObject = {
@@ -61,6 +63,7 @@ const AddLocationForm = () => {
         } else if (!newLocation) {
             alert('Location not added');
         }
+        setIsLoading(false);
     }
 
     return (
@@ -69,73 +72,79 @@ const AddLocationForm = () => {
                 <div className='mainContent'>
                     {/* Navbar here */}
                     <NavbarComponent accountType={userInfo.accountType} />
-                    <Container className="my-5">
-                        <Row className="justify-content-center">
-                            <Col className="col-6 col-md-4 addLocationForm p-4">
-                                <h2 className="text-center fw-bold">Add New Location</h2>
-                                <Form>
-                                    <Form.Group className="mb-3" controlId="locationName">
-                                        <Form.Label visuallyHidden>Location Name</Form.Label>
-                                        <Form.Control
-                                            className="inputFieldStyle"
-                                            type="text"
-                                            placeholder="Name of Location"
-                                            onChange={e => { setLocationName(e.target.value) }} />
-                                    </Form.Group>
-                                    <Form.Group className="mb-3" controlId="address">
-                                        <Form.Label visuallyHidden>Address</Form.Label>
-                                        <Form.Control
-                                            className="inputFieldStyle"
-                                            type="text"
-                                            placeholder="Address"
-                                            onChange={e => { setAddress(e.target.value) }} />
-                                    </Form.Group>
-                                    <Form.Group className="mb-3" controlId="city">
-                                        <Form.Label visuallyHidden>City</Form.Label>
-                                        <Form.Control
-                                            className="inputFieldStyle"
-                                            type="text"
-                                            placeholder="City"
-                                            onChange={e => { setCity(e.target.value) }} />
-                                    </Form.Group>
-                                    <Row>
-                                        <Col className="col-8">
-                                            <Form.Group className="mb-3" controlId="state">
-                                                <Form.Label visuallyHidden>State</Form.Label>
+                    {
+                        isLoading ?
+                            <LoadingPageComponent />
+                            :
+                            <Container className="my-5">
+                                <Row className="justify-content-center">
+                                    <Col className="col-6 col-md-4 addLocationForm p-4">
+                                        <h2 className="text-center fw-bold">Add New Location</h2>
+                                        <Form>
+                                            <Form.Group className="mb-3" controlId="locationName">
+                                                <Form.Label visuallyHidden>Location Name</Form.Label>
                                                 <Form.Control
                                                     className="inputFieldStyle"
                                                     type="text"
-                                                    placeholder="State"
-                                                    onChange={e => { setState(e.target.value) }} />
+                                                    placeholder="Name of Location"
+                                                    onChange={e => { setLocationName(e.target.value) }} />
                                             </Form.Group>
-                                        </Col>
-                                        <Col className="col-4">
-                                            <Form.Group className="mb-3" controlId="zip">
-                                                <Form.Label visuallyHidden>Zip</Form.Label>
+                                            <Form.Group className="mb-3" controlId="address">
+                                                <Form.Label visuallyHidden>Address</Form.Label>
                                                 <Form.Control
                                                     className="inputFieldStyle"
                                                     type="text"
-                                                    placeholder="Zip"
-                                                    onChange={e => { setZip(e.target.value) }} />
+                                                    placeholder="Address"
+                                                    onChange={e => { setAddress(e.target.value) }} />
                                             </Form.Group>
-                                        </Col>
-                                    </Row>
-                                    <Row className="mt-3">
-                                        <Col className="col-6">
-                                            <Button
-                                                className="btn bg-danger border-danger w-100"
-                                                onClick={handleCancel}>Cancel</Button>
-                                        </Col>
-                                        <Col className="col-6">
-                                            <Button
-                                                className="btn lightBlueBG w-100"
-                                                onClick={handleAddLocation}>Submit</Button>
-                                        </Col>
-                                    </Row>
-                                </Form>
-                            </Col>
-                        </Row>
-                    </Container>
+                                            <Form.Group className="mb-3" controlId="city">
+                                                <Form.Label visuallyHidden>City</Form.Label>
+                                                <Form.Control
+                                                    className="inputFieldStyle"
+                                                    type="text"
+                                                    placeholder="City"
+                                                    onChange={e => { setCity(e.target.value) }} />
+                                            </Form.Group>
+                                            <Row>
+                                                <Col className="col-8">
+                                                    <Form.Group className="mb-3" controlId="state">
+                                                        <Form.Label visuallyHidden>State</Form.Label>
+                                                        <Form.Control
+                                                            className="inputFieldStyle"
+                                                            type="text"
+                                                            placeholder="State"
+                                                            onChange={e => { setState(e.target.value) }} />
+                                                    </Form.Group>
+                                                </Col>
+                                                <Col className="col-4">
+                                                    <Form.Group className="mb-3" controlId="zip">
+                                                        <Form.Label visuallyHidden>Zip</Form.Label>
+                                                        <Form.Control
+                                                            className="inputFieldStyle"
+                                                            type="text"
+                                                            placeholder="Zip"
+                                                            onChange={e => { setZip(e.target.value) }} />
+                                                    </Form.Group>
+                                                </Col>
+                                            </Row>
+                                            <Row className="mt-3">
+                                                <Col className="col-6">
+                                                    <Button
+                                                        className="btn bg-danger border-danger w-100"
+                                                        onClick={handleCancel}>Cancel</Button>
+                                                </Col>
+                                                <Col className="col-6">
+                                                    <Button
+                                                        className="btn lightBlueBG w-100"
+                                                        onClick={handleAddLocation}>Submit</Button>
+                                                </Col>
+                                            </Row>
+                                        </Form>
+                                    </Col>
+                                </Row>
+                            </Container>
+                    }
+
                 </div>
                 <Footer />
             </div>
